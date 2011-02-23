d29f1f1adeae98f3c0a9e9319273e478a0fc2164
techdharma-ajay <ajay@techdharma.com>
2011-02-23 14:57:28 +0530

    Updated repository

---
+++ lightbox-video/gallery.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    	<link type="text/css" rel="stylesheet" media="all" href="http://www.dev.thinkwellgroup.com/themes/garland/style.css" />
+        <link rel="shortcut icon" href="/misc/favicon.ico" type="image/x-icon" />
+    	<title>Video-Image Gallery</title>
+        <script src="http://www.dev.thinkwellgroup.com/js/jsall.js"></script>
+    	<script type="text/javascript">
+			$(document).ready(function(){
+                if($("#banner-nav"))
+                {
+                	$("#banner-nav").slideShow({
+                    	item : 0,
+                    	busy : false,
+                    	paused : true,
+                    	interval : null,
+                    	animationSpeed : 1,
+                    	animationInterval : 10,
+                    	menuOpacityOff : 1,
+                    	menuOpacityOn : 1,
+                    	jQuery_banner : "banner-nav",
+					 	jQuery_banner_menu : "menu-banner"					
+                	});
+                }
+            });
+        </script>
+  	</head>
+    
+    <body>
+    	<div id="slide_div" class="0">      
+        	<div id="wrapper">
+        		<div id="main-wrapper">
+            		<div id="center">
+                		<div id="node-14" class="node">  
+  							<div class="content clear-block">								
+								<div class="project-details" id="projects">
+    								<div class="pdetails-middle">
+    									<div class="gallery-image" id="slideshow">
+											<div class="Banner">
+                								<div class="Banner-tl"></div>
+                								<div class="Banner-tr"><div></div></div>
+                								<div class="Banner-bl"><div></div></div>
+                								<div class="Banner-br"><div></div></div>
+                								<div class="Banner-tc"><div></div></div>
+                								<div class="Banner-bc"><div></div></div>
+                								<div class="Banner-cl"><div></div></div>
+                								<div class="Banner-cr"><div></div></div>
+                								<div class="Banner-cc"></div>
+                								<div id="banner-nav" class="banner-nav" >
+					                    			<a class="banner" href="javascript:void(0)" style="z-index: 1; opacity: 0;">
+                        								<img src="../misc/project_gallery/20100622_011344_SNL-1-HERO.jpg" id="newal" />
+                    								</a>
+                                        			<a class="banner" href="javascript:void(0)" style="z-index: 1; opacity: 0;">
+                        								<img src="../misc/project_gallery/20100622_011344_SNL-2.jpg" id="newal" />
+                    								</a>
+                                        			<a class="banner" href="javascript:void(0)" style="z-index: 1; opacity: 0;">
+                        								<img src="../misc/project_gallery/20100622_011344_SNL-3.jpg" id="newal" />
+                    								</a>
+                                        			<a class="banner" href="javascript:void(0)" style="z-index: 1; opacity: 0;">
+                        								<img src="../misc/project_gallery/20100622_011344_SNL-4.jpg" id="newal" />
+                    								</a>
+                                        			<a class="banner" href="javascript:void(0)" style="z-index: 1; opacity: 0;">
+                        								<img src="../misc/project_gallery/20100622_011344_SNL-5.jpg" id="newal" />
+                    								</a>
+                                        			<a class="banner" href="javascript:void(0)" style="z-index: 1; opacity: 0;">
+                        								<img src="../misc/project_gallery/20100622_011344_SNL-6.jpg" id="newal" />
+                    								</a>
+                                        			<a class="banner" href="javascript:void(0)" style="z-index: 1; opacity: 0;">
+                        								<img src="../misc/project_gallery/20100622_011344_SNL-7.jpg" id="newal" />
+                    								</a>
+                                        			<div class="menu-banner" id="menu-banner" style="">
+                        								<a href="javascript:bannerNav.playpause();" style="display: none;" class="playpause">Play / Pause</a>
+                        								<a href="javascript:bannerNav.prev();" style="display: none;" class="prev">Previous</a>
+                        								<a style="" class="item" href="javascript:bannerNav.toBanner(0);">&nbsp;</a>
+                                                        <a style="" class="item" href="javascript:bannerNav.toBanner(1);">&nbsp;</a>
+                                                        <a style="" class="item" href="javascript:bannerNav.toBanner(2);">&nbsp;</a>
+                                                        <a style="" class="item" href="javascript:bannerNav.toBanner(3);">&nbsp;</a>
+                                                        <a style="" class="item" href="javascript:bannerNav.toBanner(4);">&nbsp;</a>
+                                                        <a style="" class="item" href="javascript:bannerNav.toBanner(5);">&nbsp;</a>
+                                                        <a style="" class="item" href="javascript:bannerNav.toBanner(6);">&nbsp;</a>
+                                                        <a href="javascript:bannerNav.next();" style="display: none;" class="next">Next</a>                    </div>
+            										</div>
+            									</div>
+        									</div>
+        									<div class="" id="tabs-area-content">
+            									<div class="project-details tabs-content" id="readTabContent">
+                                                	<b>Client: Broadway Video</b><br /><br />Partnering with Broadway Video, 
+                                                    Thinkwell is creating and producing an all-new kind of “live” attraction that 
+                                                    brings to life over thirty-five years of classic characters and sketches from 
+                                                    one of the longest- running and most groundbreaking comedy television shows in 
+                                                    history, engaging guests with highly interactive media content and immersive 
+                                                    technology.
+                                              	</div>
+        									</div>
+        									<div class="gradient">&nbsp;</div>
+    									</div>    
+    									<div class="pdetails-lower">		        
+        									<div class="lower-right" id="projectsLinks">		            
+            									<span class="white-text" >
+                                					<!--<a id="various1" href="#inline1" title="ATTRACTION CONCEPT">-->ATTRACTION CONCEPT<!--</a>-->
+                									<div style="display: none;">
+                    									<div id="inline1" style="width:400px;height:100px;overflow:auto;"></div>
+                									</div>
+                								</span>
+        									</div>
+    									</div>
+									</div>
+                             	</div>
+            				</div>        
+           				</div>
+        			</div>            
+        		</div>
+	</body>   
+</html>
